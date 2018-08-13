5ad9773478f5125a0fffbac74765827864ca7e0d
Michael Giannini <therealmichaelgiannini@gmail.com>
2018-08-13 16:37:00 -0500

    added a navbar, still need to style it

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import SimpleCard from './components/SimpleCard.jsx'
+import ButtonAppBar from './components/ButtonAppBar.jsx'
 import './style/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <SimpleCard />
+        <ButtonAppBar />
       </div>
     );
   }
